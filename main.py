f3703a2afdb5b13a57ba273d75abe45810987b22
ggardusi <gustavo.gardusi@gmail.com>
2019-07-05 13:46:52 -0300

    debug

--- main.py
+++ main.py
@@ -24,18 +24,26 @@ class SNMP:
       stderr = subprocess.STDOUT,
     )
     
+  def build(self):
+    documents = []
+    for ip in self.ips:
+      for i in range(len(self.info[ip])):
+        documents.append(self.info[ip][i])
+    return documents
+    
   def run(self):
     for ip in self.ips:
       self.info[ip] = []
       for line in self.snmpRun(ip, 'ifDescr'):
         self.info[ip].append({
+          'ip': ip,
           'interface': line.split('=')[-1].strip()
         })
       for oid in self.oids:
         v = self.snmpRun(ip, self.oids[oid])
-        print(v)
         for i in range(len(self.info[ip])):
           self.info[ip][i][oid] = v[i].split('=')[-1].strip()
+        print('ip: ' + ip)
         pprint.pprint(self.info[ip])
       return
     
@@ -46,6 +54,21 @@ class SNMP:
       '-c', self.snmpCommunity,
       ip, oid
     ]).stdout.decode('utf-8').strip().split('\n')
+    
+def insert_documents(documents, database_credentials, database_name, table_name, table_info):
+  db = mySQL(
+    database_credentials = database_credentials,
+    database_name = database_name,
+  )
+  db.create_table(
+    table_info = table_info,
+    table_name = table_name,
+  )
+  db.insert_into(
+    table_info = table_info,
+    table_name = table_name,
+    values = documents,
+  )
 
 def main():
   config = readJson(current_filepath + 'config.json')
@@ -55,6 +78,13 @@ def main():
     oids = readJson(current_filepath + 'oid.json')
   )
   magic.run()
+  insert_documents(
+    documents = magic.build(),
+    database_credentials = readJson(config['databaseCredentialsFilepath']),
+    database_name = config['database_name'],
+    table_name = config['table_name'],
+    table_info = readJson(current_filepath + 'tableInfo.json')
+  )
 
 def readJson(filepath):
   with open(filepath, 'rb') as file:

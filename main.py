3a75bfe3b4ac2b9ad82b53607b728be3848b6e13
ggardusi <gustavo.gardusi@gmail.com>
2019-07-05 12:18:10 -0300

    testing

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import json
 import os
+import subprocess
 
 current_filepath = os.path.realpath(
   os.path.join(os.getcwd(), os.path.dirname(__file__))
 ) + '/'
 
 def main():
-  config = read_json(current_filepath + 'config.json')
-  ips = read_json(config['ipsFilepath'])
+  config = readJson(current_filepath + 'config.json')
+  ips = readJson(config['ipsFilepath'])
+  for ip in ips:
+    aux = localAccessRun([
+      '/usr/bin/snmpwalk',
+      '-v', '2c',
+      '-c', 'V01prO2005',
+      ip,
+      '.1.3.6.1.2.1.2.2.1.2'
+    ])
+    print(aux)
 
 def readJson(filepath):
   with open(filepath, 'rb') as file:
     return json.load(file, encoding = 'utf-8')
+    
+def localAccessRun(command):
+  return subprocess.run(
+    args = command,
+    stdout = subprocess.PIPE,
+    stderr = subprocess.STDOUT,
+  )
 
 main()

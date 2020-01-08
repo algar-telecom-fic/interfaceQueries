d55a68e27828e608af6d5ecc57b041d255f0a76c
pediogo <pedrodiogo219@gmail.com>
2020-01-08 17:37:46 -0300

    added hosts notion of time

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import os
 import pprint
 import subprocess
 import sys
+import datetime
 os.sys.path.append('/home/gardusi/github/sql_library/')
 from sql_json import mySQL
+from dateutil.parser import parse
+
+def processDate(s):
+  s = " ".join(s.split('=')[-1].split(':')[-4:])
+  s = " ".join(s.split(',')).strip()
+  s = (" ".join(s.split(' ')[0].split('-') + s.split(' ')[1:])).strip()
+  s = s.split(' ')
+  s = '{}-{}-{} {}:{}:{}{}:{}'.format(s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7])
+
+  return parse(s)
+
 
 current_filepath = os.path.realpath(
   os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -12,36 +24,43 @@ current_filepath = os.path.realpath(
 
 class SNMP:
   info = {}
-  
+
   def __init__(self, snmpCommunity, ips, oids):
     self.snmpCommunity = snmpCommunity
     self.ips = ips
     self.oids = oids
-    
+
   def build(self):
     documents = []
     for ip in self.ips:
       for i in range(len(self.info[ip])):
         documents.append(self.info[ip][i])
       return documents
-      
+
   def getValue(self, s):
     return s.split('=')[-1].split(':')[-1].strip()
-    
+
   def localAccessRun(self, command):
     return subprocess.run(
       args = command,
       stdout = subprocess.PIPE,
       stderr = subprocess.STDOUT,
     )
-    
+
   def run(self):
     for ip in self.ips:
       self.info[ip] = []
+      localDate = self.snmpRun(ip, ".1.3.6.1.2.1.25.1.2.0") # The host's notion of the local date and time of day.
+      localDate = processDate(localDate[0])
+
+      dia = datetime.datetime.now()
+
       for line in self.snmpRun(ip, 'ifDescr'):
         self.info[ip].append({
           'ip': ip,
-          'interface': self.getValue(line)
+          'interface': self.getValue(line),
+          'hrSystemDate': localDate,
+          'dia': dia,
         })
       for oid in self.oids:
         v = self.snmpRun(ip, self.oids[oid])
@@ -50,15 +69,15 @@ class SNMP:
       print('ip: ' + ip)
       pprint.pprint(self.info[ip])
       return
-    
+
   def snmpRun(self, ip, oid):
     return self.localAccessRun([
       '/usr/bin/snmpwalk',
-      '-v', '2c',
+      '-v2c',
       '-c', self.snmpCommunity,
       ip, oid
     ]).stdout.decode('utf-8').strip().split('\n')
-    
+
 def insert_documents(documents, database_credentials, database_name, table_name, table_info):
   db = mySQL(
     database_credentials = database_credentials,
@@ -82,6 +101,10 @@ def main():
     oids = readJson(current_filepath + 'oid.json')
   )
   magic.run()
+  docs = magic.build()
+  for d in docs:
+      print(d)
+
   insert_documents(
     documents = magic.build(),
     database_credentials = readJson(config['databaseCredentialsFilepath']),
